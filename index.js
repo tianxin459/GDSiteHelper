0d951a55fa4e6a0c4f6b71c1435aefc71bd77c2b
tianxin459 <ellis.tx.et@gmail.com>
2016-07-20 09:26:08 +0800

    remove comment code

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ var pageMod = require("sdk/page-mod");
 // Create a page-mod
 // It will run a script whenever a ".org" URL is loaded
 // The script replaces the page contents with a message
-/*
-pageMod.PageMod({
-  include: "*.org",
-  contentScript: 'console.log("page mod matches")'
-});
-*/
 
 pageMod.PageMod({
     include:/.*\/greendot\/.*/,
